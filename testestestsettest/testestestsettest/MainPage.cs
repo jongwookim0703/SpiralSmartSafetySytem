a4eb1bbba5f70c0764aa19ec48ab12cb6e19d951
KimSUYEON97 <calicitine97@naver.com>
2021-09-08 18:07:01 +0900

    main화면 생성 first 화면

--- testestestsettest/testestestsettest/MainPage.cs
+++ testestestsettest/testestestsettest/MainPage.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace testestestsettest
 {
     public partial class MainPage : Form
@@ -15,6 +16,33 @@ namespace testestestsettest
         public MainPage()
         {
             InitializeComponent();
+
+            //login form 호출
+            /*LogIn Login = new LogIn();
+            Login.ShowDialog();
+
+            //tssUserName.Text = Login.Tag.ToString();//tag는 배열처럼 사용 불가능
+            if (Login.Tag.ToString() == "FAIL")
+            {
+                Application.ExitThread();
+                Application.Exit();
+                System.Environment.Exit(0);
+            }*/
+
+
+            //this.stbExit.Click += new System.EventHandler(this.stbExit_Click); //버튼에 이벤트 추가 //stbExit_Click이 자동 생성 안된 경우 작성
+
+            //this.stbClose.Click += new System.EventHandler(this.stbClose_Click);//버튼 닫기 이벤트 추가
+
+            //this.M_SYSTEM_DropDownItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.drp) 
+            //drp == M_SYSTEM_DropDownItemClicked 직접만든것 //메뉴 클릭 이벤트 추가
         }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            tssTimer.Text = DateTime.Now.ToString();//breaktime 걸면 1초마다 break 발생
+        }
+
+        
     }
 }
